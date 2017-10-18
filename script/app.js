ef519c9751eff454c710db8828564770c918f633
anabiz <anabizconcept@gmail.com>
2017-10-18 14:29:11 +0100

    updating html and js files

--- script/app.js
+++ script/app.js
@@ -1,1 +1,19 @@
-app.js
+var arr = ['first item', 'second item is longer than the third one', 
+           'third longish item'];
+
+var lgth = 0;
+var longest;
+
+for(var i=0; i < arr.length; i++){
+    if(arr[i].length > lgth){
+        var lgth = arr[i].length;
+        longest = arr[i];
+    }      
+} 
+
+alert(longest);
+
+
+
+
+
